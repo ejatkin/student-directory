02c30497b837c52fbc77cfc5e980483177450fff
Elizabeth Atkin <ejatkin@gmail.com>
2016-11-04 16:28:21 +0000

    14 - exercises 1-5

--- typos.rb
+++ typos.rb
@@ -11,20 +11,22 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def process(selection)
   case selection
     when "1"
-      input_students
+      input_students_names
     when "2"
       show_students
     when "3"
       save_students
+      puts "Students saved."
     when "4"
       load_students
+      puts "Loading students from file."
     when "9"
       exit
     else
@@ -32,18 +34,33 @@ def process(selection)
   end
 end
 
-def input_students
+def input_students_names
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = STDIN.gets.chomp
+  @name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while !@name.empty? do
   # add the student hash to the array
-  @students << {name: name, cohort: :november}
-  puts "Now we have #{@students.count} students"
+  input_student_cohort
+  add_students
+  puts "Student successfully added. Now we have #{@students.count} students"
   # get another name from the user
-  name = STDIN.gets.chomp
+  @name = STDIN.gets.chomp
+  end
+end
+
+def input_student_cohort
+  puts "What is the student's cohort?"
+    @cohort = gets.chomp.to_sym
+    if @cohort.empty?
+      @cohort = :november
+    end
+    until [:January, :February, :March, :April, :May, :June, :July,
+    :August, :September, :October, :November, :December].include?(@cohort)
+    puts "I'm sorry, that answer wasn't recognised. Please enter the full name 
+    of the month."
+    @cohort = gets.chomp.to_sym
   end
 end
 
@@ -68,32 +85,53 @@ def print_footer
   puts "Overall, we have #{@students.count} great students."
 end
 
+def choose_file
+  puts "Please specify a filename to open"
+  puts "If no file is selected, students.csv will be opened by default"
+  filename = gets.chomp
+  #if the file exists
+  if filename.exists?
+  #then assign it to the @file variable which appears later
+  @file = filename
+  # else if the file doesn't exist
+  elsif !filename.exists?
+  #assign filename to the students.csv
+  @file = "students.csv"
+  end
+end
+
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "w")
+  File.open(@file, "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
-    file.puts csv_line
+    @file.puts csv_line
   end
-  file.close
+  @file.close
 end
 
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+def load_students
+  File.open(@file, "r")
+  @file.readlines.each do |line|
+  @name, @cohort = line.chomp.split(',')
+    add_students
   end
-  file.close
+  @file.close
+end
+
+def add_students
+  @students << {name: @name, cohort: @cohort.to_sym}
 end
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
+  if filename.nil? # get out of the method if it isn't given
+    load_students
+  elsif File.exists?(filename) # if it exists
+    @file = filename
+    load_students
       puts "Loaded #{@students.count} from #{filename}"
   else # if it doesn't exist
     puts "Sorry, #{filename} doesn't exist."
